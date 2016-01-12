3d272318195026a60dc754a170af9597c4f97ace
Quy Nguyen <qnguyen50@csc.com>
2016-01-12 15:15:41 +0700

    Add cache using cloudant

--- server.js
+++ server.js
@@ -13,6 +13,24 @@ app.get("/fizzbuzz_range/:from/:to", function(req, res) {
 	});
 });
 
+var CacheFizzBuzz = require("./cachefizzbuzz");
+var dbURL = "";
+if (process.env.VCAP_SERVICES) {
+	var env = JSON.parse(process.env.VCAP_SERVICES);
+	dbURL = env.cloudantNoSQLDB[0].credentials.url + "/fizzbuzz";
+} else {
+	dbURL = "http://localhost:5984/fizzbuzz";
+}
+
+app.get("/cache_fizzbuzz_range/:from/:to", function(req, res) {
+	var cachefizzbuzz = new CacheFizzBuzz(dbURL);
+	var from = req.params.from;
+	var to = req.params.to;
+	cachefizzbuzz.fizzBuzzRange(from, to, function(data) {
+		res.send(data);
+	});
+});
+
 var server_port = process.env.VCAP_APP_PORT || 3000;
 var server_host = process.env.VCAP_APP_HOST || "localhost";
 var server = app.listen(server_port, server_host, function() {
